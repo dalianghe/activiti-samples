d570a0ab9014a97c3529909345c0841305f9b095
dalianghe <276420284@qq.com>
2017-12-20 17:41:02 +0800

    增加事物

--- activiti-spring-boot-mysql/src/main/java/com/xingguang/activiti/service/impl/LeaveServiceImpl.java
+++ activiti-spring-boot-mysql/src/main/java/com/xingguang/activiti/service/impl/LeaveServiceImpl.java
@@ -9,6 +9,7 @@ import org.activiti.engine.TaskService;
 import org.activiti.engine.task.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.xingguang.activiti.service.ILeaveService;
 
@@ -22,6 +23,7 @@ public class LeaveServiceImpl implements ILeaveService {
 	private TaskService taskService;
 
 	@Override
+	@Transactional
 	public void startProcess() {
 		Map<String , Object> vars = new HashMap<String , Object>();
 		vars.put("applyUser", "zhangsan");
@@ -29,6 +31,7 @@ public class LeaveServiceImpl implements ILeaveService {
 	}
 
 	@Override
+	@Transactional
 	public List<Task> getTasks(String assignee) {
 		return taskService.createTaskQuery().taskAssignee(assignee).list();
 	}
